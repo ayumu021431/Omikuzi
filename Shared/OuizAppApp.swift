37104e93e2167ca41a7a7d205523592757482a31
ayumu021431 <ayumu_skywalker@icloud.com>
2022-03-19 17:17:17 +0900

    Initial Commit

---
+++ Shared/OuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OuizAppApp.swift
+//  Shared
+//
+//  Created by asora on 2022/03/19.
+//
+
+import SwiftUI
+
+@main
+struct OuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
